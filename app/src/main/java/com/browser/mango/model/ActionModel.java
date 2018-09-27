2de8cf9351a77198f04332001d11b14ac2a48499
pf.developer@outlook.com <pf.developer@outlook.com>
2018-09-27 13:41:47 +0800

    drawable refactor

--- app/src/main/java/com/browser/mango/model/ActionModel.java
+++ app/src/main/java/com/browser/mango/model/ActionModel.java
@@ -1,10 +1,13 @@
 package com.browser.mango.model;
 
+import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.browser.mango.R;
+import com.mango.seed.Utilities;
 
 /**
  * @author tic
@@ -12,8 +15,23 @@ import com.browser.mango.R;
  */
 public class ActionModel {
 
+    private MaterialDialog mMenuDialog;
+
     public View inflate(LayoutInflater inflater, ViewGroup parent) {
         return inflater.inflate(R.layout.view_bottom_action_bar, parent, false);
+    }
+
+    public void showMenuDialog(Activity activity) {
+        mMenuDialog = new MaterialDialog
+                .Builder(activity)
+                .customView(R.layout.dialog_menu, false)
+                .show();
+    }
+
+    public void disMenu() {
+        if (Utilities.isNotNull(mMenuDialog) && mMenuDialog.isShowing()) {
+            mMenuDialog.dismiss();
+        }
     }
 
 }
